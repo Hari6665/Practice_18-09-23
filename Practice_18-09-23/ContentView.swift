dd2af0393355db8e042271494487238747d7f75a
Harikrishnan K M <harikunjath407@gmail.com>
2023-09-18 11:02:01 +0530

    Initial Commit

---
+++ Practice_18-09-23/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Practice_18-09-23
+//
+//  Created by Harikrishnan K M on 18/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Practice_18-09-23/Practice_18_09_23App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_18_09_23App.swift
+//  Practice_18-09-23
+//
+//  Created by Harikrishnan K M on 18/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Practice_18_09_23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
